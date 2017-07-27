a038ad1a615d0fb9f5d7a41092a8da68a153ce9b
devefx <devefx@163.com>
2017-07-27 14:39:37 +0800

    Add the missed submission

---
+++ validator-web/src/main/java/org/devefx/validator/http/reader/json/GsonHttpMessageReader.java
@@ -0,0 +1,41 @@
+package org.devefx.validator.http.reader.json;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.devefx.validator.http.MediaType;
+import org.devefx.validator.http.reader.AbstractHttpMessageReader;
+import org.devefx.validator.http.reader.HttpMessageNotReadableException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class GsonHttpMessageReader extends AbstractHttpMessageReader<Object> {
+
+	protected Gson gson;
+	
+	public GsonHttpMessageReader() {
+		this(new GsonBuilder());
+	}
+	
+    public GsonHttpMessageReader(GsonBuilder builder) {
+    	super(MediaType.ALL);
+    	this.gson = builder.create();
+	}
+    
+	@Override
+	protected boolean supports(Class<?> clazz) {
+		return true;
+	}
+
+	@Override
+	protected Object readInternal(Class<? extends Object> clazz, HttpServletRequest request)
+			throws IOException, HttpMessageNotReadableException {
+		
+		InputStream in = request.getInputStream();
+		return gson.fromJson(new InputStreamReader(in), clazz);
+	}
+}
---
+++ validator-web/src/main/java/org/devefx/validator/util/ServletUtils.java
@@ -0,0 +1,27 @@
+package org.devefx.validator.util;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ServletUtils {
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void extractUrlParams(MultiValueMap multiValueMap, HttpServletRequest request) {
+		Map<String, String[]> parameterMap = request.getParameterMap();
+        for (Map.Entry<String, String[]> entry: parameterMap.entrySet()) {
+        	String key = entry.getKey();
+        	String[] values = entry.getValue();
+        	for (String value : values) {
+        		multiValueMap.add(key, value);
+			}
+        }
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static MultiValueMap extractUrlParams(HttpServletRequest request) {
+		MultiValueMap valueMap = new LinkedMultiValueMap<>();
+		extractUrlParams(valueMap, request);
+		return valueMap;
+	}
+}
